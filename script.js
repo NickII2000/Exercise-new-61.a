8bf88bd37c60ccab4057227a2bd2922e46213a62
Nick <Nick_II2000@mail.ru>
2022-06-01 22:14:46 +0600

    3-th commit

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function amountOfPages(summary) {
     while (summary > 0) {
         summary -= String(++i).length;
     }
-
     return i;
 }
 
+console.log(amountOfPages(5));
+console.log(amountOfPages(25));
 console.log(amountOfPages(1095));
+console.log(amountOfPages(185));
